13a762689d39f9e1736c275bba4a2f0c89f65a28
Poggerv2 <poggervtwo@gmail.com>
2025-10-02 17:27:31 -0300

    .

--- scripts/jsons_to_mongodb.py
+++ scripts/jsons_to_mongodb.py
@@ -3,12 +3,12 @@ import json
 from pymongo import MongoClient
 from pymongo.errors import BulkWriteError
 
-MONGO_USER = os.getenv("MONGO_USER", "Poggerv2")
-MONGO_PASSWORD = os.getenv("MONGO_PASSWORD", "lr64370l9cv7")
-MONGO_HOST = os.getenv("MONGO_HOST", "mongo")
-MONGO_PORT = int(os.getenv("MONGO_PORT", 27017))
-MONGO_DB = os.getenv("MONGO_DB", "financial_news")
-MONGO_COLLECTION = os.getenv("MONGO_COLLECTION", "articles")
+MONGO_USER = os.getenv("MONGO_USER")
+MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
+MONGO_HOST = os.getenv("MONGO_HOST")
+MONGO_PORT = int(os.getenv("MONGO_PORT"))
+MONGO_DB = os.getenv("MONGO_DB")
+MONGO_COLLECTION = os.getenv("MONGO_COLLECTION")
 DATA_DIR = "/app/data/raw"
 
 def load_json_files_to_mongo():
@@ -18,7 +18,7 @@ def load_json_files_to_mongo():
 	)
 	db = client[MONGO_DB]
 	collection = db[MONGO_COLLECTION]
-	print(f"🔍 Conectado a MongoDB en {MONGO_HOST}:{MONGO_PORT}, db={MONGO_DB}, collection={MONGO_COLLECTION}")
+	print(f"Conectado a MongoDB en {MONGO_HOST}:{MONGO_PORT}, db={MONGO_DB}, collection={MONGO_COLLECTION}")
 
 	for filename in os.listdir(DATA_DIR):
 		if filename.endswith(".json"):
@@ -36,7 +36,7 @@ def load_json_files_to_mongo():
 				if data:
 					try:
 						result = collection.insert_many(data, ordered=False)
-						print(f"✅ Insertados {len(result.inserted_ids)} documentos desde {filename}")
+						print(f"Insertados {len(result.inserted_ids)} documentos desde {filename}")
 					except BulkWriteError as bwe:
 						inserted_count = bwe.details.get("nInserted", 0)
 						dup_count = len(data) - inserted_count
